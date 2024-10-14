bac22c72282c20a02e45f3df2fadfa76b185dc08
Aloisio Guedes <169170722+aloisiox@users.noreply.github.com>
2024-10-14 00:07:31 -0300

    12.py
    alterações na função

--- 12.py
+++ 12.py
@@ -1,8 +1,10 @@
+x = float(input('Digite um valor em celsius: '))
+y = float(input('Digite um valor em Fahrenheit: '))
+
 def converter(x,y):
     celsius = float(x * 1.8) + 32
     print(f'{celsius:.2f} fahrenheit')
     fahrenheit = float(y - 32) / 1.8
     print(f'{fahrenheit:.2f} celsius')
 
-converter(32, 60)
-    
+converter(x, y)
